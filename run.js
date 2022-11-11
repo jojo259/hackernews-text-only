74bc60fc2aea30049af41ed642a61026eef63bf8
jojo259 <95320642+jojo259@users.noreply.github.com>
2022-11-11 20:22:37 +0000

    style and url hash data storing and sorting buttons

--- run.js
+++ run.js
@@ -2,24 +2,41 @@
 
 console.log("running");
 
+let atStoryId = 0;
+let atCommentId = 0;
+
 let commentIndexWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--comment-indent-width").replace("px", ""));
 
-initialLoad();
-
 let storiesDiv = document.getElementById("stories");
-
 let storyTitleDiv = document.getElementById("storytitle");
 let storyInfoDiv = document.getElementById("storyinfo");
 let storyTextDiv = document.getElementById("storytext");
 let commentsDiv = document.getElementById("comments");
 
-async function getApi(urlPath) {
-	let fullUrl = "https://hacker-news.firebaseio.com/v0/" + urlPath;
+let curSortingType = "top";
 
-	return fetch(fullUrl)
-	.then((response) => response.json())
-	.then((apiData) => {
-		return apiData;
+readHash();
+
+reloadStories(curSortingType, true);
+
+function reloadStories(scrollToCur = false) {
+	updateHash();
+	storiesDiv.innerHTML = "";
+	getApi(curSortingType + "stories.json").then(topStories => {
+		console.log(`got ${topStories.length} top story ids`);
+
+		for (let curStoryId of topStories.slice(0, 50)) {
+			getApi(`item/${curStoryId}.json`).then(curStory => {
+				let curStoryElem = getStoryElem(curStory);
+				storiesDiv.appendChild(curStoryElem);
+				if (scrollToCur && curStory.id == atStoryId) {
+					console.log("scrolling to and opening story from hash")
+					curStoryElem.scrollIntoView();
+					openStory(curStory);
+				}
+				console.log(`added story by ${curStory.by}`);
+			});
+		};
 	});
 }
 
@@ -27,7 +44,7 @@ function displayStory(curStory) { // also probably not xss safe
 	storyTitleDiv.innerHTML = curStory.title ? curStory.title : "notitle"; // HTML probably not necessary for titles
 	storyTitleDiv.href = curStory.url;
 	storyInfoDiv.innerText = getStoryInfo(curStory);
-	storyTextDiv.innerHTML = curStory.text ? curStory.text : ""; 
+	storyTextDiv.innerHTML = curStory.text ? curStory.text : "";
 }
 
 function getStoryInfo(curStory) {
@@ -37,12 +54,11 @@ function getStoryInfo(curStory) {
 function getStoryElem(curStory) {
 	let storyDiv = document.createElement("div");
 
+	storyDiv.id = "story-" + curStory.id;
 	storyDiv.classList.add("story");
 
 	storyDiv.onclick = function() {
-		commentsDiv.innerHTML = "";
-		appendComments(curStory.kids, commentsDiv, 0);
-		displayStory(curStory);
+		openStory(curStory);
 	};
 
 	storyDiv.innerText += curStory.title;
@@ -55,7 +71,8 @@ function getCommentElem(curComment, atLevel) {
 	let commentDiv = document.createElement("div");
 
 	commentDiv.classList.add("comment");
-	commentDiv.style.marginLeft = (atLevel + 1) * commentIndexWidth + "px";
+	commentDiv.id = "comment-" + curComment.id;
+	commentDiv.style.marginLeft = atLevel * commentIndexWidth + "px";
 
 	commentDiv.innerText += `${curComment.by} ${prettyTimeStr(curComment.time)}`;
 
@@ -64,6 +81,21 @@ function getCommentElem(curComment, atLevel) {
 	commentDiv.appendChild(commentTextDiv);
 
 	return commentDiv;
+}
+
+function openStory(curStory) {
+	let hashAtCommentId = getHashKey("atcomment"); // unushed
+
+	if (curStory.kids) {
+		commentsDiv.innerHTML = "";
+		appendComments(curStory.kids, commentsDiv, 0)
+	}
+	else {
+		commentsDiv.innerHTML = "No comments.";
+	}
+	displayStory(curStory);
+	atStoryId = curStory.id;
+	updateHash();
 }
 
 function appendComments(kidIds, toElement, atLevel) {
@@ -81,17 +113,14 @@ function appendComments(kidIds, toElement, atLevel) {
 	}
 }
 
-function initialLoad() {
-	getApi("topstories.json").then(topStories => {
-	console.log(`got ${topStories.length} top stories`);
+function updateHash() {
+	window.location.hash = `atstory=${atStoryId}&atcomment=${atCommentId}&sorting=${curSortingType}`;
+}
 
-	for (let curStoryId of topStories.slice(0, 50)) {
-			getApi(`item/${curStoryId}.json`).then(curStory => {
-				storiesDiv.appendChild(getStoryElem(curStory));
-				console.log(`added story by ${curStory.by}`);
-			});
-		}
-	});
+function getHashKey(getKey) { // works fine when all parameter values dont have special chars which they currently don't
+	if (window.location.hash.includes(getKey)) {
+		return window.location.hash.split(getKey + "=")[1].split("&")[0];
+	}
 }
 
 function prettyTimeStr(givenEpoch) {
@@ -136,4 +165,28 @@ function prettyTimeStr(givenEpoch) {
 	else {
 		return `${timeDiff} ${timeWord} ago`
 	}
+}
+
+function readHash() {
+	let hashSortingType = getHashKey("sorting");
+	if (hashSortingType) {
+		console.log(`hash sorting ${hashSortingType}`);
+		curSortingType = hashSortingType;
+	}
+
+	let hashAtStoryId = getHashKey("atstory");
+	if (hashAtStoryId) {
+		console.log(`hash at story ${hashAtStoryId}`);
+		atStoryId = parseInt(hashAtStoryId);
+	}
+}
+
+async function getApi(urlPath) {
+	let fullUrl = "https://hacker-news.firebaseio.com/v0/" + urlPath;
+
+	return fetch(fullUrl)
+	.then((response) => response.json())
+	.then((apiData) => {
+		return apiData;
+	});
 }
